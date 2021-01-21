32fc047ba388d7d0f48a0e6cb8cb666326a455de
Tatiana Kedrova <Baletrot@gmail.com>
2021-01-21 22:16:33 +0200

    added several event listeners

--- script.js
+++ script.js
@@ -3,3 +3,33 @@ const inputField = document.querySelector("#search");
 const deleteBtn = document.querySelector("#clear");
 
 
+inputField.addEventListener('input', () => {
+    if(inputField.value === '') {
+        deleteBtn.style.display = "none";
+    } else {
+        deleteBtn.style.display = "inline-block";
+    }
+    
+});
+
+deleteBtn.addEventListener('click', () => {
+    inputField.value = '';
+    deleteBtn.style.display = "none";
+});
+
+button.addEventListener('click', (event) => {
+    if(inputField.value === '') {
+        button.disabled = true;
+    } else {
+        alert(inputField.value);
+    }
+});
+
+const alertMethod = ()=> {
+    const div = document.createElement('div');
+    div.className = 
+};
+
+    
+
+    
